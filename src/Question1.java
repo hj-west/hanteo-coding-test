00d38a1434375f41059e6361d3b09ce5a58e3eaf
h.j_west <guswl9814@gmail.com>
2025-02-09 21:30:26 +0900

    change classes name

--- src/Question1.java
+++ src/Question1.java
@@ -1,4 +1,4 @@
-public class Test2 {
+public class Question1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
--- src/Question2.java
+++ src/Question2.java
@@ -1,4 +1,4 @@
-public class Test1 {
+public class Question2 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
